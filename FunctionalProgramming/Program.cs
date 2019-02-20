f452d72588333fb3c69b9fcc536f9693ab56e4f0
chiccomotta <cristiano.motta@hotmail.it>
2019-02-20 22:27:12 +0100

    Added Stopwatch example

--- FunctionalProgramming/Program.cs
+++ FunctionalProgramming/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Diagnostics;
 using System.Threading;
 
 namespace FunctionalProgramming
@@ -7,9 +8,30 @@ namespace FunctionalProgramming
     {
         static void Main(string[] args)
         {
+            Func<string> action = () =>
+            {
+                string tmp = string.Empty;
+
+                for (int k = 0; k < 1000; k++)
+                {
+                    for (int i = 0; i < 10000; i++)
+                    {
+                        tmp = i.ToString();
+                    }
+                }
+
+                return tmp;
+            };
+
+            var (duration, result) = StopwatchWrapper.Time<string>(action);
+
+            Console.WriteLine($"duration: {duration} - result: {result}");
+            Console.ReadLine();
+            return;
+
             ParallelizePureFunctions.ParallelizeOperations();
 
-            while (true)            
+            while (true)
             {
                 MutableAndImmutable.MutateState();
                 MutableAndImmutable.NotMutateState();
---
+++ FunctionalProgramming/StopwatchWrapper.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Text;
+
+namespace FunctionalProgramming
+{
+    public class StopwatchWrapper
+    {
+        public static (double elapsedTime, T result) Time<T>(Func<T> f)
+        {
+            Stopwatch sw = new Stopwatch();
+            sw.Start();
+
+            T t = f();
+
+            sw.Stop();
+            return (sw.ElapsedMilliseconds, t);
+        }
+    }
+}
